ec905526de4324ae504fcd19db70705ce342b269
Piotr Sipiński <psipinski@gmail.com>
2019-03-11 15:24:09 +0100

    b

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Voting App</title>
+	 <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class=" text-center">
+
+
+<p> </p>
+
+
+
+
+<h1>Głosowanie w sprawie zmiany budżetu</h1>
+
+<div class="container">
+<button type="button" class="btn btn-success btn-lg btn-block"><h1>ZA</h1></button>
+<button type="button" class="btn btn-danger btn-lg btn-block"><h1>PRZECIW</h1></button>
+<button type="button" class="btn btn-warning btn-lg btn-block"><h1>WSZTRZYMUJE SIĘ</h1></button>
+</div>
+
+
+
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ live.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Voting App</title>
+	 <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container text-center">
+
+
+<p> </p>
+
+
+ <table class="table table-bordered">
+  <tr>
+    <th>Radny</th>
+    <th>Wynik</th>
+
+  </tr>
+  <tr class="table-success">
+    <td>Marek Piekarski</td>
+    <td><button type="button" class="btn btn-success">ZA</button></td>
+
+  </tr>
+    <tr class="table-danger">
+    <td>Roman Roman</td>
+    <td><button type="button" class="btn btn-danger">PRZECIW</button></td>
+
+  </tr>
+    <tr class="table-warning">
+    <td>Zenon Benon</td>
+    <td><button type="button" class="btn btn-warning">WSTRZYMUJE SIĘ</button></td>
+
+  </tr>
+
+</table> 
+<button type="button" class="btn btn-info"><H1>LOGOWANIE </H1></button>
+</div>
+
+
+
+
+
+
+
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
